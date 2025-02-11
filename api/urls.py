1113b4f7d3e599be1154e64da30ec06ebf6588f9
Ermeson23 <joseenem35@gmail.com>
2025-02-11 03:17:21 +0000

    Update server

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from .views import FeedbackCreateView, UserProgressView
 
 urlpatterns = [
     path('feedback/', FeedbackCreateView.as_view(), name='feedback-create'),
+    path('feedback/', DoubtCreateView.as_view(), name='doubt-create'),
     path('progress/', UserProgressView.as_view(), name='user-progress'),
 ]
--- simulaMathBack/settings.py
+++ simulaMathBack/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-*gv7dj5q4*-%y5zrp197m$6!vg2ope66%a)rfqo+=ht+f6#!-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+"ermesonribeiro.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'api',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -57,14 +60,22 @@ DJOSER = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "https://simula-math.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'simulaMathBack.urls'
 
